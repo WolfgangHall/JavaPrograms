bd470d99c7675781946a9ed4f1e5f9d9ddd4a018
Wolfgang <johnalexander.w.hall@gmail.com>
2016-12-18 14:49:55 -0500

    extra practice with OOP concepts / added a child healthy burger class

---
+++ OOPchallenge/src/com/wolfgang/HealthyBurger.java
@@ -0,0 +1,42 @@
+package com.wolfgang;
+
+/**
+ * Created by johna on 12/18/2016.
+ */
+public class HealthyBurger extends Burger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy", "Brown Rye", meat, price);
+    }
+
+    public void addHealthAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+
+    public void addHealthAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+
+    @Override
+    public double itemizeBurger() {
+        double hamburgerPrice = super.itemizeBurger();
+
+        if (this.healthyExtra1Name != null) {
+            hamburgerPrice += this.healthyExtra1Price;
+            System.out.println("Added " + this.healthyExtra1Name + " for an extra " + healthyExtra1Price);
+        }
+        if (this.healthyExtra2Name != null) {
+            hamburgerPrice += this.healthyExtra2Price;
+            System.out.println("Added " + this.healthyExtra2Name + " for an extra " + healthyExtra2Price);
+        }
+
+        return hamburgerPrice;
+    }
+}
--- OOPchallenge/src/com/wolfgang/Main.java
+++ OOPchallenge/src/com/wolfgang/Main.java
@@ -8,8 +8,12 @@ public class Main {
 	    burger.addHamburgerAddition1("Tomato", 0.25);
 	    burger.addHamburgerAddition2("Lettuce", 0.50);
 	    burger.addHamburgerAddition3("Cheese", 1.12);
-//        burger.addHamburgerAddition4("Mushrooms", 0.75);
         System.out.println("Total Burger Price is " + burger.itemizeBurger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Beef", 4.30);
+        healthyBurger.addHamburgerAddition1("Tomato", 0.25);
+        healthyBurger.addHealthAddition1("Lentils", 4.41);
+        System.out.println("Total healthy burger price is " + healthyBurger.itemizeBurger());
 
     }
 }
