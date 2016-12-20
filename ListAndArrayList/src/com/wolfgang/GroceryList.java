b13561fb3fd39cde9ddaa9cbb04d806098e53d66
Wolfgang <johnalexander.w.hall@gmail.com>
2016-12-20 17:34:42 -0500

    understanding the core concepts of arraylists

---
+++ ListAndArrayList/src/com/wolfgang/GroceryList.java
@@ -0,0 +1,63 @@
+package com.wolfgang;
+
+import java.util.ArrayList;
+
+/**
+ * Created by johna on 12/20/2016.
+ */
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        // set of methods associated with ArrayLists
+        // add() allows us to add on to our list
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " item(s) in your grocery list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            // using i + 1 as lists are 0 indexed
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(String currentItem, String newItem) {
+        int position = findItem(currentItem);
+        if (position >= 0) {
+            modifyGroceryItem(position, newItem);
+        }
+    }
+
+    private void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if (position >= 0) {
+            removeGroceryItem(position);
+        }
+    }
+
+    // keeps internal number logic private
+    private void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+    private int findItem(String searchItem) {
+        return groceryList.indexOf(searchItem);
+    }
+
+    public boolean onFile(String searchItem) {
+        int position = findItem(searchItem);
+        if(position >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
